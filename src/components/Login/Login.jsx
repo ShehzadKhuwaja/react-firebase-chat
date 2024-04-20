a4b3e58ff0e353fc494fcf026b28ec13ffa13581
ShehzadKhuwaja <103369085+ShehzadKhuwaja@users.noreply.github.com>
2024-04-21 00:18:24 +0500

    registering feature completed

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,7 +2,9 @@ import React, { useState } from 'react'
 import './login.css'
 import { toast } from 'react-toastify'
 import { createUserWithEmailAndPassword } from 'firebase/auth'
-import { auth } from '../lib/firebase'
+import { auth, db } from '../lib/firebase'
+import { doc, setDoc } from "firebase/firestore"
+import upload from '../lib/upload'
 
 const Login = () => {
 
@@ -10,6 +12,8 @@ const Login = () => {
         file: null,
         url: ''
     })
+
+    const [loading, setLoading] = useState(false)
 
     const handleAvatar = e => {
         if(e.target.files[0]) {
@@ -26,16 +30,35 @@ const Login = () => {
 
     const handleRegister = async e => {
         e.preventDefault()
+        setLoading(true)
         const formData = new FormData(e.target)
 
         const { username, email, password } = Object.fromEntries(formData)
 
         try {
             const res = await createUserWithEmailAndPassword(auth, email, password)
+
+            const imgUrl = await upload(avatar.file)
+            
+            await setDoc(doc(db, "users", res.user.uid), {
+                username,
+                email,
+                avatar: imgUrl,
+                id: res.user.uid,
+                blocked: []
+            })
+
+            await setDoc(doc(db, "userchats", res.user.uid), {
+                chats: []
+            })
+
+            toast.success('Account created! You can login now!')
         }
         catch (err) {
             console.log(err)
             toast.error(err.message)
+        } finally {
+            setLoading(false)
         }
     }
 
@@ -46,7 +69,7 @@ const Login = () => {
                 <form onSubmit={handleLogin}>
                     <input type="text" placeholder='Email' name='email' />
                     <input type="passowrd" name="password" placeholder='Password' />
-                    <button>Sign In</button>
+                    <button disabled={loading}>{loading ? 'Loading': 'Sign In'}</button>
                 </form>
             </div>
             <div className="separator"></div>
@@ -61,7 +84,7 @@ const Login = () => {
                     <input type="text" placeholder='Username' name='username' />
                     <input type="text" placeholder='Email' name='email' />
                     <input type="passowrd" name="password" placeholder='Password' />
-                    <button>Sign Up</button>
+                    <button disabled={loading}>{loading ? 'Loading': 'Sign Up'}</button>
                 </form>
             </div>
         </div>
