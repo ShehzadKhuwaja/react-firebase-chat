103eaf8debf5c817a79541aa0eb0f9b7043c6a8a
ShehzadKhuwaja <shehzadkhuwaja@gmail.com>
2024-04-21 15:57:19 +0500

    state management setup zustand

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,33 @@
+import { useEffect } from "react"
 import Chat from "./components/chat/Chat"
 import Detail from "./components/detail/Detail"
 import List from "./components/list/List"
 import Login from "./components/Login/Login"
 import Notification from "./components/notification/Notification"
+import { onAuthStateChanged } from "firebase/auth"
+import { auth } from "./components/lib/firebase"
+import { useUserStore } from "./components/lib/userStore"
 
 const App = () => {
 
-  const user = false
+  const {currentUser, isLoading, fetchUserInfo } = useUserStore()
+
+  useEffect(() => {
+    const unSub = onAuthStateChanged(auth, user => {
+      fetchUserInfo(user.uid)
+    })
+
+    return () => {
+      unSub()
+    }
+  }, [fetchUserInfo])
+
+  if (isLoading) return <div className="loading">Loading...</div>
 
   return (
     <div className='container'>
       {
-        user ? (
+        currentUser ? (
           <>
           <List />
           <Chat />
